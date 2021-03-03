fe64cd4d8ec0e31d8bcb74cddfb6c1d4fdf83327
kingapl <kingapluciennik@wp.pl>
2021-03-03 18:29:39 +0100

    Edit index view and template to show all questions.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Question
+
 
 def index(request):
-    return render(request, 'polls/index.html')
+    questions = Question.objects.all()
+
+    context = {'questions': questions}
+    return render(request, 'polls/index.html', context)
