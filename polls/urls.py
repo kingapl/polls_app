a72a77487c9bdfa6ae5adcc546c9d9c1f4f71f0a
kingapl <kingapluciennik@wp.pl>
2021-03-08 20:02:05 +0100

    Add basic vote site.

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create_question/', views.create_question, name='create_question'),
     path('create_answer/<int:q_id>', views.create_answer, name='create_answer'),
+    path('vote/<int:q_id>', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Question
+from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 
 
@@ -37,3 +37,10 @@ def create_answer(request, q_id):
 
     context = {'answer_form': answer_form, 'question': question}
     return render(request, 'polls/create_answer.html', context)
+
+def vote(request, q_id):
+    question = Question.objects.get(id=q_id)
+    answers = question.answer_set.all()
+
+    context = {'question': question, 'answers': answers}
+    return render(request, 'polls/vote.html', context)
