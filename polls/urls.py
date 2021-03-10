9931d75eee8eeee9ab59a355507890c56662fc55
kingapl <kingapluciennik@wp.pl>
2021-03-10 18:17:17 +0100

    Add '/' to end the URL paths.

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_question/', views.create_question, name='create_question'),
-    path('create_answer/<int:q_id>', views.create_answer, name='create_answer'),
-    path('vote/<int:q_id>', views.vote, name='vote'),
+    path('create_answer/<int:q_id>/', views.create_answer, name='create_answer'),
+    path('vote/<int:q_id>/', views.vote, name='vote'),
 ]
